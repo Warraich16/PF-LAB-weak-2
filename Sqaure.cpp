576ea63d213dc7233bcb674f22c8d861a82eac28
zulaikha imtiaz <tu.haider138@gmail.com>
2023-01-02 11:00:14 +0500

    message

---
+++ Sqaure.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+cout << "OUR WEAK 2 LECTURE" <<endl;
+cout << "Here is my first task" <<endl;
+cout << "Square"<<endl;
+cout <<"          **************"<<endl;
+
+cout <<"          *            *" <<endl;
+cout <<"          *            *"<<endl;
+cout <<"          *            *"<<endl;
+cout <<"          *            *"<<endl;
+cout <<"          *            *"<<endl;
+cout <<"          *            *"<<endl;
+cout <<"          **************";
+}
